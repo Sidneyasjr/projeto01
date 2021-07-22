f8a25bf8723f0f3e2274945c10db6ab287769f72
Sidney Andrade <sidney.jwnior@gmail.com>
2021-07-22 18:22:45 -0300

    Front end pront. E add dos intens.

--- scripts.js
+++ scripts.js
@@ -2,19 +2,23 @@ var form = document.querySelector("#form");
 var inputItem = document.getElementById("inputItem");
 var lista = document.querySelector("#lista");
 
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     addItemNaLista();
 });
 
+
+
 function addItemNaLista() {
     var txtItem = inputItem.value;
-    var itemDefault = document.getElementById('itemDefault');
     if (txtItem) {
         var novoItem = document.createElement('li');
-        novoItem.innerHTML = itemDefault.innerHTML;
-        // novoItem.hidden = false;
         novoItem.classList.add("list-group-item")
+        novoItem.innerHTML = `<input type="checkbox" class="">
+                                <span class=""></span>
+                                <button type="button" class="btn btn-sm btn-danger excluir float-right"><i
+                                        class="bi bi-trash"></i></button>`
         novoItem.querySelector("span").innerHTML = txtItem;
         lista.appendChild(novoItem);
         inputItem.value = "";
